cc3d4c4a789f842bfff639c4f0848662cc1cc42a
Daa Nkwa <akrasikonadu@yahoo.com>
2025-05-01 10:46:19 +0000

    innitial commit

---
+++ contextapp/src/components/Navbar.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+import { ThemeContext } from '../contexts/ThemeContext';
+
+class Navbar extends Component {
+  static contextType = ThemeContext;
+  render() {
+    console.log(this.context);
+    const { isLightTheme, light, dark } = this.context;
+    const theme = isLightTheme ? light : dark;
+    return (
+      <nav style= {{background: theme.ui, color: theme.syntax}}>
+
+<h1> Context App </h1>
+<ul>
+  <li><a href="/">Home</a></li>
+  <li><a href="/">About</a></li>
+  <li><a href="/">Contact</a></li>
+</ul>
+      </nav>
+      
+    )
+  }
+}
+
+export default Navbar;
