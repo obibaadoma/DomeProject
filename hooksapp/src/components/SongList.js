57fc6eea24892444fe9803a61d58d5e404dc08f8
Daa Nkwa <akrasikonadu@yahoo.com>
2025-05-03 13:29:11 +0000

    further addition

--- hooksapp/src/components/SongList.js
+++ hooksapp/src/components/SongList.js
@@ -1,14 +1,32 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import { v4 as uuidv4 } from 'uuid';
+
+const uuid = uuidv4;
 
 const SongList = () => {
+  const [songs, setSongs] = useState([
+    { title: 'this wild darkness', id: 1 },
+    { title: 'memory gospel', id: 2 },
+    { title: 'Song 3', id: 3 }
+  ]);
+  const [age, setAge] = useState(20);
+  const addSong = () => { 
+    setSongs([...songs, { title: 'new song', id: uuid() }]);
+
+  }
+      useEffect(() => {
+        console.log('useEffect hook ran', songs);
+      })
   return (
     <div className="song-list">
       <h2>Song List</h2>
       <ul>
-        <li>this wild darkness</li>
-        <li>memory gospel</li>
-        <li>Song 3</li>
+        {songs.map(song => (
+          <li key={song.id}>{song.title}</li>
+        ))}
       </ul>
+    <button onClick={addSong}> Add a song</button>
+    <button onClick={()=>{setAge(age +1)}} >Add 1 to age: {}</button>
     </div>
   );
 }
